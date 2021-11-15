1dd5087989d46ca3dfb2778aef6f12eebf211c44
Joshua Asare <maroon@colorelephant.com>
2021-11-15 23:35:32 +0000

    added a close button to close comment form

--- src/features/Comments/CommmentForm.tsx
+++ src/features/Comments/CommmentForm.tsx
@@ -9,27 +9,41 @@ interface Props {
   ) => void;
   data?: CommentResult | null;
   onSubmit?: () => void;
+  onClose?: () => void;
 }
-const CommmentForm: React.FC<Props> = ({ addComment, data, onSubmit }) => {
+const CommmentForm: React.FC<Props> = ({
+  addComment,
+  data,
+  onSubmit,
+  onClose,
+}) => {
   const [text, setText] = useState("");
   return (
     <div className="comment-form">
       <textarea
         onChange={(ev) => setText(ev.currentTarget.value)}
         className="textarea"
+        inputMode="text"
       />
-      <button
-        className="button"
-        onClick={() => {
-          const accPath = data?.path
-            ? data?.path + "." + "firstChild"
-            : "firstChild";
-          text && addComment && addComment(text, accPath);
-          onSubmit && onSubmit();
-        }}
-      >
-        Add comment
-      </button>
+      <span>
+        <button
+          className="add-button"
+          onClick={() => {
+            const accPath = data?.path
+              ? data?.path + "." + "firstChild"
+              : "firstChild";
+            text && addComment && addComment(text, accPath);
+            onSubmit && onSubmit();
+          }}
+        >
+          Add comment
+        </button>
+        {onClose && (
+          <button className="close-button" onClick={onClose}>
+            close
+          </button>
+        )}
+      </span>
     </div>
   );
 };
